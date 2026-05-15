98945a55f8fb8d49d3e7dfe90d9f41c4885cfc13
What42Pizza <burg.what42@gmail.com>
2026-05-15 14:10:48 -0500

    fix amd crash

--- shaders/program/deferred99.glsl
+++ shaders/program/deferred99.glsl
@@ -1,12 +1,12 @@
 #ifdef FSH
 
 void main() {
-	vec2 output = texelFetch(MISC_DATA_TEXTURE, texelcoord, 0).rg;
+	vec2 data = texelFetch(MISC_DATA_TEXTURE, texelcoord, 0).rg;
 	
-	if (texelcoord == ivec2(0.0)) output = vec2(1.0);
+	if (texelcoord == ivec2(0.0)) data = vec2(1.0);
 	
 	/* RENDERTARGETS: 10 */
-	gl_FragData[0] = vec4(output, 0.0, 1.0);
+	gl_FragData[0] = vec4(data, 0.0, 1.0);
 }
 
 #endif
